fa520a8326a9dc52f691ea6aed74d0ca6afcf500
Jeff Rossi <jrossi@intouchgps.com>
2023-09-20 16:24:22 -0400

    type is keyword

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,4 +1,5 @@
 class Material < ApplicationRecord
+	self.inheritance_column = nil
 	enum type: { consumable: 0, projectile: 1, medicine: 2, resistance: 3, damage_negation: 4, grease: 5, runes: 6, multiplayer: 7 }
 
 	has_many :recipes, through: :recipe_materials
